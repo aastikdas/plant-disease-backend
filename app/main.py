2faeb46c29843b90ade699d0ffbe9eed6f184133
aastikdas <aastikdas126@gmail.com>
2025-07-09 15:41:40 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",  # Vite local dev
-    "https://plant-disease-frontend-psi.vercel.app/",
+    "https://plant-disease-detection-aastik.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
